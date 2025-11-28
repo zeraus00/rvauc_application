1ad8f33709c841e2830fbeeb5ca1a4a123e0b878
steins0668 <archelausagaton@gmail.com>
2025-11-29 01:55:30 +0800

    feat: add AuthenticationException

---
+++ app/src/main/java/com/example/rfid/auth/exceptions/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.example.rfid.auth.exceptions;
+
+public class AuthenticationException extends Exception{
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
