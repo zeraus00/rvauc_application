6af96e0ce0d23434a32cd854e13ad30c8dfb9857
steins0668 <archelausagaton@gmail.com>
2026-04-24 12:47:52 +0800

    feat: add role field to ClassList schema

--- app/src/main/java/com/example/rfid/features/enrollments/schemas/classlist/ClassList.java
+++ app/src/main/java/com/example/rfid/features/enrollments/schemas/classlist/ClassList.java
@@ -3,6 +3,7 @@ package com.example.rfid.features.enrollments.schemas.classlist;
 import java.util.List;
 
 public class ClassList {
+    public String role;
     public List<ClassListElement> classes;
 
     public ClassList() {}
