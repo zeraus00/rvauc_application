46fc473393e34317d2e84745b0e0d20caa74fbb9
steins0668 <archelausagaton@gmail.com>
2026-04-25 23:25:03 +0800

    feat: add class weekly schedule schemas

---
+++ app/src/main/java/com/example/rfid/features/enrollments/schemas/classweeklyschedule/ClassWeeklySchedule.java
@@ -0,0 +1,8 @@
+package com.example.rfid.features.enrollments.schemas.classweeklyschedule;
+
+import java.util.List;
+
+public class ClassWeeklySchedule {
+    public List<WeeklyScheduleItem> schedule;
+    public ClassWeeklySchedule() {}
+}
---
+++ app/src/main/java/com/example/rfid/features/enrollments/schemas/classweeklyschedule/WeeklyScheduleItem.java
@@ -0,0 +1,11 @@
+package com.example.rfid.features.enrollments.schemas.classweeklyschedule;
+
+public class WeeklyScheduleItem {
+    public int id;
+    public String weekDay;
+    public int weekDayNumeric;
+    public String startTime;
+    public String endTime;
+
+    public WeeklyScheduleItem() {}
+}
